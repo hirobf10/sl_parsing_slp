7cd4c6ba038c7852624cc93054ca8b893d47e819
Hiroyuki Otomo <hiro.10030201@gmail.com>
2022-02-13 00:45:42 +0900

    Add scripts

---
+++ src/dep_parser.py
@@ -0,0 +1,314 @@
+import argparse
+from typing import List, Tuple
+from collections import defaultdict
+import random
+
+import numpy as np
+
+
+def load_dataset(path, encoding="utf-8"):
+    with open(path, encoding=encoding) as f:
+        dataset = f.readlines()
+    return dataset
+
+
+class Dataset:
+    def __init__(self, dataset: List[str]):
+        """
+        The format of dataset should be CoNLL.
+        Tab-separated columns, sentences separated by space.
+        each line contains: ID, Word, Base, POS, POS2, ?, Head, Type.
+        """
+        self.data = self._format(dataset)
+
+    def __len__(self):
+        return len(self.data)
+
+    def __getitem__(self, key):
+        if isinstance(key, slice):
+            return [self.data[i] for i in range(*key.indices(len(self.data)))]
+        elif isinstance(key, int):
+            if key < 0:
+                key += len(self.data)
+            if key < 0 or key >= len(self.data):
+                raise IndexError
+            return self.data[key]
+        else:
+            raise TypeError
+
+    def _format(self, dataset: List[str]):
+        batch = []
+        for i in range(len(dataset)):
+            if dataset[i] == "\n":
+                first = i - int(dataset[i - 1].split("\t")[0])
+                batch.append(
+                    [line.split("\n")[0].split("\t") for line in dataset[first:i]]
+                )
+        return batch
+
+
+class FeatureExtractor:
+    def __init__(self, dataset: Dataset, lr=0.01, topn=30, init_func=None):
+        self.dataset = dataset
+        self.lr = lr
+        self.topn = topn
+        self.init_func = init_func
+        self.features = None
+        self.weight = None
+
+    def get_weights(self):
+        self.features = self._get_features()
+        self.weights = self._update_weights()
+        return self.weights
+
+    def _count_features(self):
+        cnt = defaultdict(lambda: defaultdict(int))
+        for batch in self.dataset:
+            for i, data_i in enumerate(batch):
+                cnt["word"][data_i[1]] += 1
+                cnt["pos"][data_i[3]] += 1
+                cnt["dep"][data_i[-1]] += 1
+                if data_i[-1] == "ROOT":
+                    cnt["pos_pos"][f"{data_i[3]}_ROOT"] += 1
+                    cnt["word_pos"][f"{data_i[1]}_ROOT"] += 1
+                    cnt["dep"][f"{data_i[-1]}_ROOT"] += 1
+                else:
+                    head_idx = i - int(data_i[0]) + int(data_i[6])
+                    head = batch[head_idx]
+                    cnt["word_word"][f"{data_i[1]}_{head[1]}"] += 1
+                    cnt["pos_pos"][f"{data_i[3]}_{head[3]}"] += 1
+                    cnt["word_pos"][f"{data_i[1]}_{head[3]}"] += 1
+                    cnt["pos_word"][f"{data_i[3]}_{head[1]}"] += 1
+                    cnt["dep_word"][f"{data_i[-1]}_{head[1]}"] += 1
+                    cnt["dep_pos"][f"{data_i[-1]}_{head[3]}"] += 1
+        return cnt
+
+    def _get_features(self):
+        """
+        This function extract features for graph-based parser.
+        Features include word form, pos, combination of pos and word form.
+        """
+        cnt = self._count_features()
+        features = []
+        for feat_type in cnt.keys():
+            if feat_type == "pos_pos":
+                for feat, v in cnt[feat_type].items():
+                    if v >= 5:
+                        features.append(feat)
+            else:
+                for feat, v in sorted(
+                    cnt[feat_type].items(), key=lambda i: i[1], reverse=True
+                )[: self.topn]:
+                    features.append(feat)
+        return features
+
+    def _init_weights(self):
+        weights = defaultdict(int)
+        for feat in self.features:
+            if self.init_func:
+                weights[feat] = self.init_func()
+            else:
+                weights[feat] = random.random()
+        return weights
+
+    def _update_weights(self):
+        w = self._init_weights()
+        for batch in self.dataset:
+            score = defaultdict(int)
+            for i, data_i in enumerate(batch):
+                for j, data_j in enumerate(batch):
+                    if i == j:
+                        continue
+                    score[j] += w[data_i[1]]
+                    score[j] += w[data_i[3]]
+                    score[j] += w[data_i[-1]]
+                    score[j] += w[f"{data_i[1]}_{data_j[1]}"]
+                    score[j] += w[f"{data_i[1]}_{data_j[3]}"]
+                    score[j] += w[f"{data_i[3]}_{data_j[1]}"]
+                    score[j] += w[f"{data_i[3]}_{data_j[3]}"]
+                    score[j] += w[f"{data_i[-1]}_{data_j[1]}"]
+                    score[j] += w[f"{data_i[-1]}_{data_j[3]}"]
+                score["ROOT"] += w[f"{data_i[1]}_ROOT"]
+                score["ROOT"] += w[f"{data_i[3]}_ROOT"]
+                score["ROOT"] += w[f"{data_i[-1]}_ROOT"]
+                if len(score) == 0:
+                    continue
+                head_pred = sorted(score.items(), key=lambda i: i[1], reverse=True)[0][
+                    0
+                ]
+                head_true = data_i[6]
+                if head_pred == "ROOT":
+                    if data_i[-1] != "ROOT":
+                        w[data_i[1]] -= self.lr
+                        w[data_i[3]] -= self.lr
+                        w[f"{data_i[1]}_ROOT"] -= self.lr
+                        w[f"{data_i[3]}_ROOT"] -= self.lr
+                        w[f"{data_i[-1]}_ROOT"] -= self.lr
+                elif head_pred != head_true:
+                    data_j = batch[head_pred]
+                    w[data_i[1]] -= self.lr
+                    w[data_i[3]] -= self.lr
+                    w[data_i[-1]] -= self.lr
+                    w[f"{data_i[1]}_{data_j[1]}"] -= self.lr
+                    w[f"{data_i[1]}_{data_j[3]}"] -= self.lr
+                    w[f"{data_i[3]}_{data_j[1]}"] -= self.lr
+                    w[f"{data_i[3]}_{data_j[3]}"] -= self.lr
+                    w[f"{data_i[-1]}_{data_j[1]}"] -= self.lr
+                    w[f"{data_i[-1]}_{data_j[3]}"] -= self.lr
+        return w
+
+
+class MSTParser:
+    def __init__(self, extractor):
+        self.w = extractor.get_weights()
+
+    def __call__(self, batch):
+        return self.parse(batch)
+
+    def parse(self, batch: List[str]):
+        scores = self._score(batch)
+        best_edges = self._get_best_edges(scores)
+        scores = self._subtract(scores, best_edges)
+        best_edges = self._get_best_edges(scores)
+        status, cycles = self._involveCycle([edge for edge, score in best_edges])
+        if status:
+            best_edges = self._remove_cycles(scores, cycles, best_edges)
+        return [edge for edge, _ in best_edges[1:]]
+
+    def evaluate(self, dataset: Dataset):
+        UAS_SUM = 0
+        for batch in dataset:
+            # Parse a sentence
+            scores = self._score(batch)
+            best_edges = self._get_best_edges(scores)
+            scores = self._subtract(scores, best_edges)
+            best_edges = self._get_best_edges(scores)
+            status, cycles = self._involveCycle([edge for edge, score in best_edges])
+            if status:
+                best_edges = self._remove_cycles(scores, cycles, best_edges)
+            y_test = self._extract_test(batch)
+            y_pred = [edge for edge, score in best_edges[1:]]
+            UAS_SUM += self._get_UAS(y_test, y_pred)
+        return UAS_SUM / len(dataset)
+
+    def _score(self, batch: List[str]):
+        N = len(batch)
+        scores = np.ones([N + 1, N + 1]) * -1e3
+        for i in range(1, N + 1):
+            for j in range(N + 1):
+                if i == j:
+                    continue
+                score = 0
+                score += self.w[batch[i - 1][1]]  # word
+                score += self.w[batch[i - 1][3]]  # pos
+                score += self.w[batch[i - 1][-1]]
+                if j == 0:
+                    score += self.w[f"{batch[i-1][1]}_ROOT"] + 1
+                    score += self.w[f"{batch[i-1][3]}_ROOT"] + 1
+                    score += self.w[f"{batch[i-1][-1]}_ROOT"] + 1
+                else:
+                    score += self.w[f"{batch[i-1][1]}_{batch[j-1][1]}"]
+                    score += self.w[f"{batch[i-1][1]}_{batch[j-1][3]}"]
+                    score += self.w[f"{batch[i-1][3]}_{batch[j-1][1]}"]
+                    score += self.w[f"{batch[i-1][3]}_{batch[j-1][3]}"]
+                    score += self.w[f"{batch[i-1][-1]}_{batch[j-1][1]}"]
+                    score += self.w[f"{batch[i-1][-1]}_{batch[j-1][3]}"]
+                scores[i, j] = score
+        return scores
+
+    def _get_best_edges(self, scores: np.ndarray):
+        #         ignore 0-th row because it would contain scores between ROOT as dependent and words as head
+        return [
+            (np.argmax(scores[i, :]), np.max(scores[i, :])) if i != 0 else (-1, -1e3)
+            for i in range(scores.shape[0])
+        ]
+
+    #         best_edges = []
+    #         root_child_idx = np.argmax(scores[:, 0])
+    #         for i in range(scores.shape[0]):
+    #             if i == root_child_idx:
+    #                 best_edges.append((0, scores[root_child_idx, 0]))
+    #             elif i == 0:
+    #                 best_edges.append((-1, -1e3))
+    #             else:
+    #                 head_idx = np.argmax(scores[i, 1:])
+    #                 best_edges.append((head_idx, scores[i, head_idx]))
+    #         return best_edges
+
+    def _subtract(self, scores: np.ndarray, best_edges: List[Tuple[int, float]]):
+        N = scores.shape[0]
+        for i in range(N):
+            for j in range(N):
+                if i == 0 or i == j:
+                    continue
+                scores[i, j] -= best_edges[i][1]
+        return scores
+
+    def _involveCycle(self, edges: List[int]):
+        memory = []
+        for dep, head in enumerate(edges):
+            dep_ = edges[head]
+            if dep == dep_ and (sorted([dep, head]) not in memory):
+                memory.append(sorted([dep, head]))
+        if memory:
+            return (True, memory)
+        else:
+            return (False, [])
+
+    def _remove_cycles(
+        self, scores: np.ndarray, cycles: List, best_edges: List[Tuple[int, float]]
+    ):
+        N = scores.shape[0]
+        for cycle in cycles:
+            scores_ = scores.copy()
+            scores_[cycle[0], cycle[1]] = -1e3
+            scores_[cycle[1], cycle[0]] = -1e3
+            node, j = divmod(np.argmax(scores_[cycle, :]), N)
+            if node == 0:
+                c_head = cycle[0]
+            else:
+                c_head = cycle[1]
+            best_edges[c_head] = (j, scores[c_head, j])
+        return best_edges
+
+    def _extract_test(self, batch: List[str]):
+        return [int(data[6]) for data in batch]
+
+    def _get_UAS(self, y_test: List[int], y_pred: List[int]):
+        assert len(y_test) == len(y_pred)
+        match_num = 0
+        for test, pred in zip(y_test, y_pred):
+            if test == pred:
+                match_num += 1
+        return match_num / len(y_test)
+
+
+def main(args):
+    random.seed(args.seed)
+    np.random.seed(seed=args.seed)
+
+    train_dataset = Dataset(load_dataset(args.train_file_path, encoding=args.encoding))
+    test_dataset = Dataset(load_dataset(args.test_file_path, encoding=args.encoding))
+    extractor = FeatureExtractor(train_dataset, topn=args.topn, lr=args.lr)
+    parser = MSTParser(extractor)
+    if args.mode == "eval":
+        print("UAC: {:.5f}".format(parser.evaluate(test_dataset)))
+    else:
+        sentence = random.choice(test_dataset)
+        print("Sentence:")
+        print(" ".join([token[1] for token in sentence]))
+        print("Prediction: ")
+        print(parser.parse(sentence))
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--train_file_path", default="data/mstparser-en-train.dep")
+    parser.add_argument("--test_file_path", default="data/mstparser-en-test.dep")
+    parser.add_argument("--encoding", default="utf-8")
+    parser.add_argument("--lr", type=float, default=0.1)
+    parser.add_argument("--topn", type=int, default=30)
+    parser.add_argument("--mode", default="eval", choices=["eval", "pred"])
+    parser.add_argument("--seed", type=int, default=42)
+
+    main(parser.parse_args())
---
+++ src/pos_tagger.py
@@ -0,0 +1,242 @@
+import argparse
+from typing import Dict, List
+from collections import defaultdict, deque
+import random
+
+
+def load_dataset(path: str, encoding="utf-8"):
+    with open(path, "r", encoding=encoding) as f:
+        dataset = [line.splitlines()[0] for line in f.readlines()]
+    return dataset
+
+
+class HMMTagger:
+    def __init__(self, bos_token="<s>", eos_token="</s>"):
+        self.bos_token = bos_token
+        self.eos_token = eos_token
+        self.prob_transition = None
+        self.prob_emission = None
+        self.pos = None
+
+    def _get_count_dict(self, dataset: List[str], split_token="_"):
+        cnt_transition = defaultdict(lambda: defaultdict(int))
+        cnt_emission = defaultdict(lambda: defaultdict(int))
+
+        for line in dataset:
+            token_pos = deque(line.split())
+            token_pos.appendleft(f"{self.bos_token}{split_token}{self.bos_token}")
+            token_pos.append(f"{self.eos_token}{split_token}{self.eos_token}")
+            token_pos = list(token_pos)
+            for i in range(len(token_pos) - 1):
+                token_i, pos_i = token_pos[i].split(split_token)
+                token_j, pos_j = token_pos[i + 1].split(split_token)
+                cnt_transition[pos_i][pos_j] += 1
+                cnt_emission[pos_i][token_i] += 1
+            else:
+                token_i, pos_i = token_pos[-1].split(split_token)
+                cnt_emission[pos_i][token_i] += 1
+
+        return cnt_transition, cnt_emission
+
+    def _get_prob_dict(self, cnt_dict: defaultdict[defaultdict[int]]):
+        prob = defaultdict(lambda: defaultdict(int))
+        for a, b_cnt in cnt_dict.items():
+            num_b = sum([cnt for cnt in b_cnt.values()])
+            for b, cnt in b_cnt.items():
+                prob[a][b] = cnt / num_b  # MLE
+        return prob
+
+    def train(self, dataset: List[str], split_token="_"):
+        """
+        Compute transition probability and emission probability with MLE
+        """
+        cnt_transition, cnt_emission = self._get_count_dict(dataset, split_token)
+        self.prob_transition = self._get_prob_dict(cnt_transition)
+        self.prob_emission = self._get_prob_dict(cnt_emission)
+
+        self.pos = list(cnt_transition.keys())
+
+    def _viterbi(self, tokens: List[str]):
+        prob_path = defaultdict(lambda: defaultdict(int))
+        backpointer = defaultdict(lambda: defaultdict(str))
+
+        # N = len(self.pos)
+        T = len(tokens)
+
+        # initialization step
+        for state in self.pos:
+            if (state not in self.prob_transition[self.bos_token]) or (
+                tokens[0] not in self.prob_emission[state]
+            ):
+                continue
+            prob_path[1][state] = (
+                self.prob_transition[self.bos_token][state]
+                * self.prob_emission[state][tokens[0]]
+            )
+            backpointer[1][state] = self.bos_token
+
+        # recursion step
+        for t in range(2, T + 1):
+            p_max = 0
+            current = None
+            pointer = None
+            for state in self.pos:
+                for state_ in self.pos:
+                    if (state not in self.prob_transition[state_]) or (
+                        tokens[t - 1] not in self.prob_emission[state]
+                    ):
+                        continue
+                    temp = (
+                        prob_path[t - 1][state_]
+                        * self.prob_transition[state_][state]
+                        * self.prob_emission[state][tokens[t - 1]]
+                    )
+                    if temp >= p_max:
+                        p_max = temp
+                        current = state
+                        pointer = state_
+            else:
+                prob_path[t][current] = p_max
+                backpointer[t][current] = pointer
+        else:
+            prob_bestpath = 0
+            pointer = None
+            for state in self.pos:
+                if state not in prob_path[T]:
+                    continue
+                if prob_path[T][state] > prob_bestpath:
+                    prob_bestpath = prob_path[T][state]
+                    pointer = state
+            bestpath_reversed = [pointer]
+            for t in range(T, 1, -1):
+                if backpointer[t][pointer] == "":
+                    bestpath_reversed.append(None)
+                else:
+                    bestpath_reversed.append(backpointer[t][pointer])
+                pointer = backpointer[t][pointer]
+
+        return list(reversed(bestpath_reversed)), prob_bestpath
+
+    def _get_metrics(self, TP: int, FN: int, FP: int):
+        prec = TP / (TP + FP)
+        recall = TP / (TP + FN)
+        f1 = 2 * prec * recall / (prec + recall)
+        return prec, recall, f1
+
+    def _get_scores(self, y_test: List[str], y_pred: List[str]):
+        assert len(y_test) == len(y_pred)
+
+        scores = defaultdict(lambda: defaultdict(int))
+        for pos_test, pos_pred in zip(y_test, y_pred):
+            if pos_test == pos_pred:
+                scores[pos_pred]["TP"] += 1
+            else:
+                scores[pos_test]["FN"] += 1
+                scores[pos_pred]["FP"] += 1
+        return scores
+
+    def _get_ppl(self, prob: float, N: int):
+        return (1 / prob) ** (1 / N)
+
+    def evaluate(self, dataset: List[str], split_token="_", return_each_metric=False):
+        """
+        Return macro precision, macro recall, macro f-score and the average of PPL.
+        The average of PPL is calculated without the sentences where the path probabilities are assigned as zeros
+        """
+
+        macro_scores = defaultdict(lambda: defaultdict(int))
+        macro_metrics = defaultdict(lambda: defaultdict(int))
+        ppl = []
+        cnt_null = 0
+        for line in dataset:
+            tokens = []
+            y_test = []
+            for token_pos in line.split():
+                token, pos = token_pos.split(split_token)
+                tokens.append(token)
+                y_test.append(pos)
+            y_pred, prob = self._viterbi(tokens)
+            if prob > 0:
+                ppl.append(self._get_ppl(prob, len(tokens)))
+            else:
+                cnt_null += 1
+            scores = self._get_scores(y_test, y_pred)
+            for pos, score_cnt in scores.items():
+                for score, cnt in score_cnt.items():
+                    macro_scores[pos][score] += cnt
+        for pos in macro_scores.keys():
+            if "TP" not in macro_scores[pos]:
+                continue
+            prec, recall, f1 = self._get_metrics(
+                macro_scores[pos]["TP"],
+                macro_scores[pos]["FN"],
+                macro_scores[pos]["FP"],
+            )
+            macro_metrics[pos]["Precision"] = prec
+            macro_metrics[pos]["Recall"] = recall
+            macro_metrics[pos]["f-score"] = f1
+        ppl_average = sum(ppl) / len(ppl)
+
+        ave_prec = sum(
+            [macro_metrics[pos]["Precision"] for pos in macro_scores.keys()]
+        ) / len(macro_scores.keys())
+        ave_recall = sum(
+            [macro_metrics[pos]["Recall"] for pos in macro_scores.keys()]
+        ) / len(macro_scores.keys())
+        ave_f1 = sum(
+            [macro_metrics[pos]["f-score"] for pos in macro_scores.keys()]
+        ) / len(macro_scores.keys())
+
+        print(f"The path probabilities of {cnt_null} sentences are assigned as zeros.")
+        print(
+            f"Precision: {ave_prec}, Recall: {ave_recall}, f-score: {ave_f1}, Average of PPL: {ppl_average}"
+        )
+        if return_each_metric:
+            return ave_prec, ave_recall, ave_f1, ppl_average, macro_metrics
+        else:
+            return ave_prec, ave_recall, ave_f1, ppl_average
+
+    def tag(self, text: str, split_token="_"):
+        """
+        Return a sequence where POS is assigned to each token, with the specified split token.
+        None is assigned when the model can not predict any POS.
+        """
+        tokens = text.split()
+        result, prob = self._viterbi(tokens)
+        return (
+            " ".join(
+                [f"{tokens[i]}{split_token}{pos}" for i, pos in enumerate(result)]
+            ),
+            prob,
+        )
+
+
+def main(args):
+    random.seed(args.seed)
+    train_data = load_dataset(args.train_file_path, encoding=args.encoding)
+    test_data = load_dataset(args.test_file_path, encoding=args.encoding)
+    tagger = HMMTagger()
+    tagger.train(train_data, args.split_token)
+    if args.mode == "eval":
+        _ = tagger.evaluate(test_data, args.split_token)
+    else:
+        sample = random.choice(test_data)
+        text = " ".join(
+            [token_pos.split(args.split_token)[0] for token_pos in sample.split()]
+        )
+        print("Sentence:")
+        print(text)
+        print("Prediction:")
+        print(tagger.tag(text, args.split_token))
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--train_file_path", default="data/wiki-en-train.norm_pos")
+    parser.add_argument("--test_file_path", default="data/wiki-en-test.norm_pos")
+    parser.add_argument("--encoding", default="utf-8")
+    parser.add_argument("--split_token", default="_")
+    parser.add_argument("--mode", default="eval", choices=["eval", "pred"])
+    parser.add_argument("--seed", type=int, default=42)
+
+    main(parser.parse_args())
