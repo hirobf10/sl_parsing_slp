6659736cb5215e7053b85629f1fbf2cabe8d08c0
Hiroyuki Otomo <hiro.10030201@gmail.com>
2022-02-15 01:14:30 +0900

    Fix the way to update parameters correctly

--- src/dep_parser.py
+++ src/dep_parser.py
@@ -1,9 +1,22 @@
+import os
 import argparse
 from typing import Callable, List, Tuple, Union
 from collections import defaultdict
 import random
 
 import numpy as np
+
+ID_TO_FEATURE = {
+    0: "ID",
+    1: "Word",
+    2: "Base",
+    3: "POS",
+    4: "POS2",
+    5: "?",
+    6: "Head",
+    7: "DepType",
+}
+FEATURE_TO_ID = {v: k for k, v in ID_TO_FEATURE.items()}
 
 
 def load_dataset(path, encoding: str):
@@ -49,177 +62,167 @@ class Dataset:
 
 class FeatureExtractor:
     dataset: Dataset
-    lr: float
+    feature_types_path: str
     topn: int
-    init_func: Callable
+    feature_types: List[str]
+    features: List[str]
 
-    def __init__(self, dataset: Dataset, lr=0.01, topn=30, init_func=None):
+    def __init__(self, dataset: Dataset, feature_types_path: str, topn=30):
         self.dataset = dataset
-        self.lr = lr
         self.topn = topn
-        self.init_func = init_func
+        self.feature_types = self._load_feature_types(feature_types_path)
         self.features = None
-        self.weight = None
 
-    def get_weights(self) -> defaultdict[int]:
-        self.features = self._get_features()
-        self.weights = self._update_weights()
-        return self.weights
+    def _load_feature_types(self, feature_path: str, encoding="utf-8") -> List[str]:
+        with open(feature_path, mode="r", encoding=encoding) as f:
+            lines = [line for line in f.read().split("\n")]
+        feature_types = []
+        for line in lines:
+            _, f_child, f_head = map(int, line.split(","))
+            if f_head == -1:
+                feature_types.append(ID_TO_FEATURE[f_child])
+            else:
+                feature_types.append(
+                    f"{ID_TO_FEATURE[f_child]}_{ID_TO_FEATURE[f_head]}"
+                )
+        return feature_types
 
     def _count_features(self) -> defaultdict[defaultdict[int]]:
         cnt = defaultdict(lambda: defaultdict(int))
-        for batch in self.dataset:
-            for i, data_i in enumerate(batch):
-                cnt["word"][data_i[1]] += 1
-                cnt["pos"][data_i[3]] += 1
-                cnt["dep"][data_i[-1]] += 1
-                if data_i[-1] == "ROOT":
-                    cnt["pos_pos"][f"{data_i[3]}_ROOT"] += 1
-                    cnt["word_pos"][f"{data_i[1]}_ROOT"] += 1
-                    cnt["dep"][f"{data_i[-1]}_ROOT"] += 1
-                else:
-                    head_idx = i - int(data_i[0]) + int(data_i[6])
-                    head = batch[head_idx]
-                    cnt["word_word"][f"{data_i[1]}_{head[1]}"] += 1
-                    cnt["pos_pos"][f"{data_i[3]}_{head[3]}"] += 1
-                    cnt["word_pos"][f"{data_i[1]}_{head[3]}"] += 1
-                    cnt["pos_word"][f"{data_i[3]}_{head[1]}"] += 1
-                    cnt["dep_word"][f"{data_i[-1]}_{head[1]}"] += 1
-                    cnt["dep_pos"][f"{data_i[-1]}_{head[3]}"] += 1
+        for sents in self.dataset:
+            for i, data_i in enumerate(sents):
+                for f_type in self.feature_types:
+                    if "_" not in f_type:
+                        cnt[f_type][data_i[FEATURE_TO_ID[f_type]]] += 1
+                    else:
+                        if data_i[-1] == "ROOT":
+                            f_child = f_type.split("_")[0]
+                            cnt[f"{f_type}"][
+                                f"{data_i[FEATURE_TO_ID[f_child]]}_ROOT"
+                            ] += 1
+                        else:
+                            head_idx = i - int(data_i[0]) + int(data_i[6])
+                            head = sents[head_idx]
+                            f_child, f_head = f_type.split("_")
+                            cnt[f_type][
+                                f"{data_i[FEATURE_TO_ID[f_child]]}_{head[FEATURE_TO_ID[f_head]]}"
+                            ] += 1
         return cnt
 
-    def _get_features(self) -> List[str]:
+    def extract(self):
         """
-        This function extract features for graph-based parser.
-        Features include word form, pos, dependency, combination of them between dependent and head
+                This function extract features for graph-based parser.
+        <<<<<<< HEAD
+                Features include word form, pos, dependency, combination of them between dependent and head
+        =======
+                Features include word form, pos, and combination of pos and word form.
+        >>>>>>> a51cafd (Fix the way to update parameters correctly)
         """
         cnt = self._count_features()
         features = []
         for feat_type in cnt.keys():
-            if feat_type == "pos_pos":
-                for feat, v in cnt[feat_type].items():
-                    if v >= 5:
-                        features.append(feat)
-            else:
-                for feat, v in sorted(
-                    cnt[feat_type].items(), key=lambda i: i[1], reverse=True
-                )[: self.topn]:
-                    features.append(feat)
-        return features
+            for feat, _ in sorted(
+                cnt[feat_type].items(), key=lambda i: i[1], reverse=True
+            )[: self.topn]:
+                features.append(feat)
+        self.features = features
 
-    def _init_weights(self) -> defaultdict[int]:
-        weights = defaultdict(int)
-        for feat in self.features:
-            if self.init_func:
-                weights[feat] = self.init_func()
-            else:
-                weights[feat] = random.random()
-        return weights
+    def save(self, feature_path: str, encoding="utf-8"):
+        with open(feature_path, mode="w", encoding=encoding) as f:
+            f.write("\n".join(self.features))
 
-    def _update_weights(self) -> defaultdict[int]:
-        w = self._init_weights()
-        for batch in self.dataset:
-            score = defaultdict(int)
-            for i, data_i in enumerate(batch):
-                for j, data_j in enumerate(batch):
-                    if i == j:
-                        continue
-                    score[j] += w[data_i[1]]
-                    score[j] += w[data_i[3]]
-                    score[j] += w[data_i[-1]]
-                    score[j] += w[f"{data_i[1]}_{data_j[1]}"]
-                    score[j] += w[f"{data_i[1]}_{data_j[3]}"]
-                    score[j] += w[f"{data_i[3]}_{data_j[1]}"]
-                    score[j] += w[f"{data_i[3]}_{data_j[3]}"]
-                    score[j] += w[f"{data_i[-1]}_{data_j[1]}"]
-                    score[j] += w[f"{data_i[-1]}_{data_j[3]}"]
-                score["ROOT"] += w[f"{data_i[1]}_ROOT"]
-                score["ROOT"] += w[f"{data_i[3]}_ROOT"]
-                score["ROOT"] += w[f"{data_i[-1]}_ROOT"]
-                if len(score) == 0:
-                    continue
-                head_pred = sorted(score.items(), key=lambda i: i[1], reverse=True)[0][
-                    0
-                ]
-                head_true = data_i[6]
-                if head_pred == "ROOT":
-                    if data_i[-1] != "ROOT":
-                        w[data_i[1]] -= self.lr
-                        w[data_i[3]] -= self.lr
-                        w[f"{data_i[1]}_ROOT"] -= self.lr
-                        w[f"{data_i[3]}_ROOT"] -= self.lr
-                        w[f"{data_i[-1]}_ROOT"] -= self.lr
-                elif head_pred != head_true:
-                    data_j = batch[head_pred]
-                    w[data_i[1]] -= self.lr
-                    w[data_i[3]] -= self.lr
-                    w[data_i[-1]] -= self.lr
-                    w[f"{data_i[1]}_{data_j[1]}"] -= self.lr
-                    w[f"{data_i[1]}_{data_j[3]}"] -= self.lr
-                    w[f"{data_i[3]}_{data_j[1]}"] -= self.lr
-                    w[f"{data_i[3]}_{data_j[3]}"] -= self.lr
-                    w[f"{data_i[-1]}_{data_j[1]}"] -= self.lr
-                    w[f"{data_i[-1]}_{data_j[3]}"] -= self.lr
-        return w
+    def load(self, feature_path: str, encoding="utf-8"):
+        with open(feature_path, mode="r", encoding=encoding) as f:
+            self.features = [line for line in f.read().split("\n")]
 
 
 class MSTParser:
     extractor: FeatureExtractor
+    w: defaultdict(int)
 
     def __init__(self, extractor: FeatureExtractor):
-        self.w = extractor.get_weights()
+        self.features = extractor.features
+        self.feature_types = extractor.feature_types
+        self.w = None
 
-    def __call__(self, batch: List[List[str]]) -> List[int]:
-        return self.parse(batch)
+    def __call__(self, sents: List[List[str]]) -> List[int]:
+        return self.parse(sents)
 
-    def parse(self, batch: List[List[str]]) -> List[int]:
-        scores = self._score(batch)
+    def _init_weights(self, init_func: Callable) -> defaultdict[int]:
+        weights = defaultdict(int)
+        for feat in self.features:
+            if init_func:
+                weights[feat] = init_func()
+            else:
+                weights[feat] = random.random()
+        return weights
+
+    def _update_weights(self, sents: List[List[str]], y_pred: List[int], lr: float):
+        y_test = self._extract_test(sents)
+        for i, (test, pred) in enumerate(zip(y_test, y_pred)):
+            pred -= 1
+            if test != pred:
+                for f_type in self.feature_types:
+                    if "_" not in f_type:
+                        self.w[f"{sents[i][FEATURE_TO_ID[f_type]]}"] -= lr
+                    else:
+                        if pred == -1:
+                            f_child = f_type.split("_")[0]
+                            self.w[f"{sents[i][FEATURE_TO_ID[f_child]]}_ROOT"] -= lr
+                        else:
+                            f_child, f_head = f_type.split("_")
+                            self.w[
+                                f"{sents[i][FEATURE_TO_ID[f_child]]}_{sents[pred][FEATURE_TO_ID[f_head]]}"
+                            ] -= lr
+
+    def parse(self, sents: List[List[str]]) -> List[int]:
+        scores = self._score(sents)
         best_edges = self._get_best_edges(scores)
         scores = self._subtract(scores, best_edges)
         best_edges = self._get_best_edges(scores)
-        status, cycles = self._involveCycle([edge for edge, score in best_edges])
+        status, cycles = self._involveCycle([edge for edge, _ in best_edges])
         if status:
             best_edges = self._remove_cycles(scores, cycles, best_edges)
         return [edge for edge, _ in best_edges[1:]]
 
+    def train(self, dataset: Dataset, init_func=None, lr=0.1, iter_num=10):
+        self.w = self._init_weights(init_func)
+        for _ in range(iter_num):
+            for sents in dataset:
+                y_pred = self.parse(sents)
+                self._update_weights(sents, y_pred, lr)
+
     def evaluate(self, dataset: Dataset) -> float:
         UAS_SUM = 0
-        for batch in dataset:
+        for sents in dataset:
             # Parse a sentence
-            scores = self._score(batch)
-            best_edges = self._get_best_edges(scores)
-            scores = self._subtract(scores, best_edges)
-            best_edges = self._get_best_edges(scores)
-            status, cycles = self._involveCycle([edge for edge, score in best_edges])
-            if status:
-                best_edges = self._remove_cycles(scores, cycles, best_edges)
-            y_test = self._extract_test(batch)
-            y_pred = [edge for edge, score in best_edges[1:]]
+            y_test = self._extract_test(sents)
+            y_pred = self.parse(sents)
             UAS_SUM += self._get_UAS(y_test, y_pred)
         return UAS_SUM / len(dataset)
 
-    def _score(self, batch: List[List[str]]) -> np.ndarray:
-        N = len(batch)
+    def _score(self, sents: List[List[str]]) -> np.ndarray:
+        N = len(sents)
         scores = np.ones([N + 1, N + 1]) * -1e3
         for i in range(1, N + 1):
             for j in range(N + 1):
                 if i == j:
                     continue
                 score = 0
-                score += self.w[batch[i - 1][1]]  # word
-                score += self.w[batch[i - 1][3]]  # pos
-                score += self.w[batch[i - 1][-1]]
-                if j == 0:
-                    score += self.w[f"{batch[i-1][1]}_ROOT"] + 1
-                    score += self.w[f"{batch[i-1][3]}_ROOT"] + 1
-                    score += self.w[f"{batch[i-1][-1]}_ROOT"] + 1
-                else:
-                    score += self.w[f"{batch[i-1][1]}_{batch[j-1][1]}"]
-                    score += self.w[f"{batch[i-1][1]}_{batch[j-1][3]}"]
-                    score += self.w[f"{batch[i-1][3]}_{batch[j-1][1]}"]
-                    score += self.w[f"{batch[i-1][3]}_{batch[j-1][3]}"]
-                    score += self.w[f"{batch[i-1][-1]}_{batch[j-1][1]}"]
-                    score += self.w[f"{batch[i-1][-1]}_{batch[j-1][3]}"]
+                for f_type in self.feature_types:
+                    if "_" not in f_type:
+                        score += self.w[sents[i - 1][FEATURE_TO_ID[f_type]]]
+                    else:
+                        if j == 0:
+                            f_child = f_type.split("_")[0]
+                            score += self.w[
+                                f"{sents[i-1][FEATURE_TO_ID[f_child]]}_ROOT"
+                            ]
+                        else:
+                            f_child, f_head = f_type.split("_")
+                            score += self.w[
+                                f"{sents[i-1][FEATURE_TO_ID[f_child]]}_{sents[j-1][FEATURE_TO_ID[f_head]]}"
+                            ]
                 scores[i, j] = score
         return scores
 
@@ -283,8 +286,8 @@ class MSTParser:
             best_edges[c_head] = (j, scores[c_head, j])
         return best_edges
 
-    def _extract_test(self, batch: List[List[str]]) -> List[int]:
-        return [int(data[6]) for data in batch]
+    def _extract_test(self, sents: List[List[str]]) -> List[int]:
+        return [int(data[6]) for data in sents]
 
     def _get_UAS(self, y_test: List[int], y_pred: List[int]) -> float:
         assert len(y_test) == len(y_pred)
@@ -301,8 +304,17 @@ def main(args):
 
     train_dataset = Dataset(load_dataset(args.train_file_path, encoding=args.encoding))
     test_dataset = Dataset(load_dataset(args.test_file_path, encoding=args.encoding))
-    extractor = FeatureExtractor(train_dataset, topn=args.topn, lr=args.lr)
+    extractor = FeatureExtractor(train_dataset, args.feature_types_path, args.topn)
+    extractor.extract()
+    extractor.save(args.feature_path)
+    # if os.path.isfile(args.feature_path):
+    #     extractor.load(args.feature_path)
+    # else:
+    #     extractor.extract()
+    #     extractor.save(args.feature_path)
+
     parser = MSTParser(extractor)
+    parser.train(test_dataset, init_func=None, lr=args.lr, iter_num=args.iter_num)
     if args.mode == "eval":
         print("UAS: {:.5f}".format(parser.evaluate(test_dataset)))
     else:
@@ -319,9 +331,12 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--train_file_path", default="data/mstparser-en-train.dep")
     parser.add_argument("--test_file_path", default="data/mstparser-en-test.dep")
+    parser.add_argument("--feature_types_path", default="data/feature-types.txt")
+    parser.add_argument("--feature_path", default="data/feature.txt")
     parser.add_argument("--encoding", default="utf-8")
     parser.add_argument("--lr", type=float, default=0.1)
     parser.add_argument("--topn", type=int, default=30)
+    parser.add_argument("--iter-num", type=int, default=10)
     parser.add_argument("--mode", default="eval", choices=["eval", "pred"])
     parser.add_argument("--seed", type=int, default=42)
 
